03a4dd390baadd46a854d80fc24a07e3eb4f17a0
eelizabethriley <eeriley@smith.edu>
2025-02-25 20:46:11 -0500

    Finish hw4

--- CSC209/Hwks/Hw4/Technical/js/animation.js
+++ CSC209/Hwks/Hw4/Technical/js/animation.js
@@ -16,35 +16,87 @@ function getSpeed() {
     return speed;
 }
 
-function getSpeed(){
-    var i;
-    var squares = document.getElementsByClassName("squares");
-    // for (i=0; i <) {
-
-    // }
-}
-
 // Wrapper for moveSquare() that animates each square
 function moveSquares(){   
     for (var i = 0; i < NUM_SQUARES; i++) {
         color = squareColors[i];
-        movesquare(color);
+        moveSquare(color);
     }
 }
 
 // Animate each individual square
-moveSquare(color){
-    var square = document.getElementById(color + "Sq");
-    var squarePos = 350; //Start square in top left corner
-    var stepId = setInterval(stepSquares, getSpeed()); //Call function according to selected speed
+function moveSquare(color){
+    var square = document.getElementById(color);
+    let squarePos = square.style.top;
+    let side = square.style.left;
+    //Strip "px" ending from the position to convert to a number
+    squarePos = squarePos.slice(0, squarePos.length-2);
+    console.log("pos", squarePos);
+    var stepId;
+    switch(squarePos){
+      case "0":
+        if (side == "0"){
+            stepId = setInterval(stepSquaresDown, getSpeed());
+            console.log("step: down");
+        }
+        else {
+            stepId = setInterval(stepSquaresDownLeft, getSpeed());
+            console.log("step: downleft", stepId);
+        }
+        break;
+      case "350":
+        if (side == "0"){
+            stepId = setInterval(stepSquaresUp, getSpeed());
+            console.log("step: ", stepId);
+        }
+        else {
+            stepId = setInterval(stepSquaresUpRight, getSpeed());
+            console.log("step: ", stepId);
+        }
+        break;
+      default:
+        console.log("default");
+        break;
+    }
     // Update position of squares to animate motion
-    function stepSquares() {
-        if (bluePos == 0) { // When the square reaches the edge of the canvas
-            clearInterval(stepBlueId); // stop moving/calling function
+    function stepSquaresDown() {
+        if (squarePos == 350) { // When the square reaches the edge of the canvas
+            console.log("STOPPED");
+            clearInterval(stepId); // stop moving/calling function
         } else { // While the square is within bounds
-            bluePos--; // Move square, updating position 
-            blueSquare.style.top = bluePos + 'px'; 
-            blueSquare.style.left = bluePos + 'px';
+            squarePos++; // Move square, updating position 
+            square.style.top = squarePos + 'px'; 
+            square.style.left = squarePos + 'px';
+        }
+    }
+    function stepSquaresDownLeft() {
+        if (squarePos == 350) { // When the square reaches the edge of the canvas
+            console.log("STOPPED");
+            clearInterval(stepId); // stop moving/calling function
+        } else { // While the square is within bounds
+            squarePos++; // Move square, updating position 
+            square.style.top = squarePos + 'px'; 
+            square.style.left = squarePos - 'px';
+        }
+    }
+    function stepSquaresUp() {
+        if (squarePos == 0) { // When the square reaches the edge of the canvas
+            console.log("STOPPED");
+            clearInterval(stepId); // stop moving/calling function
+        } else { // While the square is within bounds
+            squarePos--; // Move square, updating position 
+            square.style.top = squarePos + 'px'; 
+            square.style.left = squarePos + 'px';
+        }
+    }
+    function stepSquaresUpRight() {
+        if (squarePos == 0) { // When the square reaches the edge of the canvas
+            console.log("STOPPED");
+            clearInterval(stepId); // stop moving/calling function
+        } else { // While the square is within bounds
+            squarePos--; // Move square, updating position 
+            square.style.top = squarePos + 'px'; 
+            square.style.left = squarePos - 'px';
         }
     }
 }
@@ -93,6 +145,9 @@ function createSquares() {
     // Assign square color
     squareDiv.style.backgroundColor = color;
     // Assign initial position
+    squareDiv.style.top = positionsVertical[i] + 'px';
+    squareDiv.style.left = positionsHorizontal[i] + 'px';
+    console.log(squareDiv);
     // Assign direction of motion
     squareContainer.appendChild(squareDiv);
   }
